4c0d33a6cd9f219dfdc0aef13e7d5b3c9d4b45a0
Kim Ryeonwoo <kimlw90@keti.re.kr>
2018-06-15 10:16:21 +0900

    Changed logics

--- cpp/main_PointCloud.cpp
+++ cpp/main_PointCloud.cpp
@@ -16,23 +16,13 @@ int main(int argc, char **argv)
 	ophPointCloud* Hologram = new ophPointCloud();
 
 	Hologram->setMode(MODE_CPU); //Select CPU or GPU Processing
+	Hologram->loadPointCloud(INPUT_3D_MODEL);
+	Hologram->readConfig(INPUT_CONFIG);
 
-	if (Hologram->loadPointCloud(INPUT_3D_MODEL) == -1)
-	{
-		cerr << "Failed to load Point Cloud Data File(*.dat)" << endl;
-		return -1;
-	}
-
-	if (!Hologram->readConfig(INPUT_CONFIG))
-	{
-		cerr << "Failed to load Config Specification Data File(*.config)" << endl;
-		return -1;
-	}
-
-	double time = Hologram->generateHologram();
+	Hologram->generateHologram();
+	Hologram->encode();
 	//Encoding process required : Hologram->encodeHologram();
 	Hologram->normalize();
-	cout << "Implement Time : " << time << " sec" << endl;
 
 	Hologram->save(OUTPUT_BMP);
 
