841cf74602831afe221c269123be77342afa9fcd
shsoh <shinsh@etri.re.kr>
2018-08-02 17:03:15 +0900

    Modify config file of cascaded propagation

--- cpp/main_CascadedPropagation.cpp
+++ cpp/main_CascadedPropagation.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	OphCascadedPropagation* pCp = new OphCascadedPropagation(L"TestSpecCascadedPropagation.xml");
 	if (pCp->propagate())
-		pCp->saveIntensityAsImg(L"intensity.bmp", pCp->GetNumColors() * 8);
+		pCp->saveIntensityAsImg(L"result/intensityRGB.bmp", pCp->GetNumColors() * 8);
 
 	pCp->release();
 
