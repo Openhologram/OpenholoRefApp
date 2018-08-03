cb0e24faa50b923365c7759c0e32bacca91f62a9
shsoh <shinsh@etri.re.kr>
2018-08-03 13:27:54 +0900

    Apply OpenHolo naming rules
    Version: naming_rule_V1_180703

--- cpp/main_CascadedPropagation.cpp
+++ cpp/main_CascadedPropagation.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main()
 {
-	OphCascadedPropagation* pCp = new OphCascadedPropagation(L"TestSpecCascadedPropagation.xml");
+	ophCascadedPropagation* pCp = new ophCascadedPropagation(L"TestSpecCascadedPropagation.xml");
 	if (pCp->propagate())
-		pCp->saveIntensityAsImg(L"result/intensityRGB.bmp", pCp->GetNumColors() * 8);
+		pCp->saveIntensityAsImg(L"result/intensityRGB.bmp", pCp->getNumColors() * 8);
 
 	pCp->release();
 
