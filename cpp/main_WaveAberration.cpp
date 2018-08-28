87e4f3e2dcb7890bf71bea1e7e387e0afc261b73
김련우 <kimlw90@naver.com>
2018-08-28 14:14:25 +0900

    fixed bug

--- cpp/main_WaveAberration.cpp
+++ cpp/main_WaveAberration.cpp
@@ -10,14 +10,14 @@ int main()
 
 	ophWaveAberration* wa = new ophWaveAberration;
 
-	wa->readConfig("TestSpecAberration.xml");  // reads parameters from a configuration file
+	wa->readConfig("config/TestSpecAberration.xml");  // reads parameters from a configuration file
 	wa->accumulateZernikePolynomial();         // generates 2D complex data array of wave aberration according to parameters   
 	wa->complex_W;                            // double pointer variable of 2D complex data array of wave aberration
 	wa->resolutionX;                           // resolution in x axis of 2D complex data array of wave aberration
 	wa->resolutionY;                           // resolution in y axis of 2D complex data array of wave aberration
-	wa->saveAberration("aberration.bin");      // saves 2D complex data array of complex wave aberration into a file
+	wa->saveAberration("result/aberration.bin");      // saves 2D complex data array of complex wave aberration into a file
 
-	wa->readAberration("aberration.bin");      // reads 2D complex data array of complex wave aberration from a file
+	wa->readAberration("result/aberration.bin");      // reads 2D complex data array of complex wave aberration from a file
 	wa->complex_W;                            // double pointer variable of 2D complex data array of wave aberration
 	wa->resolutionX;                           // resolution in x axis of 2D complex data array of wave aberration
 	wa->resolutionY;                           // resolution in y axis of 2D complex data array of wave aberration
