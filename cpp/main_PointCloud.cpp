0929545b36a5006ea2d3921927491b93d12e5d29
Kim Ryeonwoo <kimlw90@naver.com>
2018-10-01 16:50:03 +0900

    Added logic for *.ohc read/write

--- cpp/main_PointCloud.cpp
+++ cpp/main_PointCloud.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 
 	ophPointCloud* Hologram = new ophPointCloud();
 
-	Hologram->setMode(MODE_CPU); //Select CPU or GPU Processing
+	Hologram->setMode(MODE_GPU); //Select CPU or GPU Processing
 	Hologram->readConfig("config/TestSpecPointCloud.xml");
 	Hologram->loadPointCloud("source/TestPointCloud_Saturn.ply");
 
@@ -17,6 +17,17 @@ int main(int argc, char **argv)
 	Hologram->encodeHologram();
 	Hologram->normalize();
 	Hologram->save("result/Result_PointCloudSample_Saturn");
+	Hologram->saveAsOhc("result/Result_PointCloudSample_Saturn");
+
+	//Hologram->loadAsOhc("result/Result_PointCloudSample_Saturn");
+	//Hologram->setMode(MODE_GPU);
+	//Hologram->setOffsetDepth(0.3);
+	//Hologram->setScale(0.01, 0.01, 0.01);
+
+	//Hologram->encodeHologram();
+	//Hologram->normalize();
+
+	//Hologram->save("result/Result_PointCloudSample_SaturnByOHC");
 
 	Hologram->release();
 
